a97e6b8291a15ed348e58e9a3f3be6960393c499
sigmarius <aamokeeva@yandex.ru>
2020-05-16 12:36:23 +0300

    Исправляет ошибки кроссбраузерности

--- gulpfile.js
+++ gulpfile.js
@@ -105,10 +105,11 @@ gulp.task("server", function () {
 
   gulp.watch("source/sass/**/*.scss", gulp.series("css"));
   gulp.watch("source/img/sprite-icons/*.svg", gulp.series("sprite", "html", "refresh"));
+  gulp.watch("source/js/*.js", gulp.series("js-min", "html", "refresh"));
   gulp.watch("source/*.html", gulp.series("html", "refresh"));
 });
 
-gulp.task("refresh", function () {
+gulp.task("refresh", function (done) {
   server.reload();
   done();
 });
